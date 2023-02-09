3cc915253e1a55c61770502b3bd4ac7d1635bd6a
Raneemq1 <raneemalqadi32@gmail.com>
2023-02-09 14:42:21 +0200

    add calc act

---
+++ app/src/main/java/com/example/fitnessapp/ui/DifferenceActivity.java
@@ -0,0 +1,14 @@
+package com.example.fitnessapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DifferenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_difference);
+    }
+}
